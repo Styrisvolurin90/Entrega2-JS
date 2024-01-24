5f422e57397d47c184fed138b1a07fedcedba5bb
Hiram Franco <hapf.58@gmail.com>
2024-01-24 17:55:43 -0600

    Primer commit

---
+++ playeras.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'Oversized Goku Black',
+        precio: 450,
+        categoria: 'Oversized'
+    },
+    {
+        id: 2,
+        nombre: 'Recto gyarados',
+        precio: 250,
+        categoria: 'Recto'
+    },
+    {
+        id: 3,
+        nombre: 'Slimfit pollito',
+        precio: 300,
+        categoria: 'Slimfit'
+    },
+    {
+        id: 4,
+        nombre: 'Mujer Goku Black',
+        precio: 250,
+        categoria: 'Mujer'
+    },
+]
